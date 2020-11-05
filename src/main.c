ea9d3ce8735f4d8a4a5caa7b4604eb86b8a5671b
Einar Thorsrud <einar.thorsrud@nordicsemi.no>
2020-11-05 14:29:28 +0100

    Indent with tabs

--- src/main.c
+++ src/main.c
@@ -23,22 +23,22 @@ int store_key_in_kmu(uint32_t slot_id)
 {
 	int ret;
 
-    uint8_t key[16] =
-    {
-        0x8b, 0xe8, 0xf0, 0x86, 0x9b, 0x3c, 0x0b, 0xa9,
-        0x7b, 0x71, 0x86, 0x3d, 0x1b, 0x9f, 0x78, 0x13
-    };
+	uint8_t key[16] =
+	{
+		0x8b, 0xe8, 0xf0, 0x86, 0x9b, 0x3c, 0x0b, 0xa9,
+		0x7b, 0x71, 0x86, 0x3d, 0x1b, 0x9f, 0x78, 0x13
+	};
 
-    ret = nrf_cc3xx_platform_kmu_write_key_slot(
-        slot_id,
-        NRF_CC3XX_PLATFORM_KMU_AES_ADDR,
-        NRF_CC3XX_PLATFORM_KMU_DEFAULT_PERMISSIONS,
-        key);
-    if (ret != NRF_CC3XX_PLATFORM_SUCCESS)
-    {
-        printk("Could not write KMU slot %i. Try erasing the board...\n", slot_id);
-        return -1;
-    }
+	ret = nrf_cc3xx_platform_kmu_write_key_slot(
+		slot_id,
+		NRF_CC3XX_PLATFORM_KMU_AES_ADDR,
+		NRF_CC3XX_PLATFORM_KMU_DEFAULT_PERMISSIONS,
+		key);
+	if (ret != NRF_CC3XX_PLATFORM_SUCCESS)
+	{
+		printk("Could not write KMU slot %i. Try erasing the board...\n", slot_id);
+		return -1;
+	}
 
 	return 0;
 }
@@ -54,58 +54,58 @@ int use_key_from_kmu(uint32_t slot_id)
    // Plaintext: abcdefghijklmnop
    uint8_t plain_text[16] =
    {
-        0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
-        0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70
+		0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+		0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70
    };
 
-    // Ciphertext: fd671e1dc1aca49124704502ea716dba
-    uint8_t cipher_expected[16] =
-    {
-        0xfd, 0x67, 0x1e, 0x1d, 0xc1, 0xac, 0xa4, 0x91,
-        0x24, 0x70, 0x45, 0x02, 0xea, 0x71, 0x6d, 0xba
-    };
+	// Ciphertext: fd671e1dc1aca49124704502ea716dba
+	uint8_t cipher_expected[16] =
+	{
+		0xfd, 0x67, 0x1e, 0x1d, 0xc1, 0xac, 0xa4, 0x91,
+		0x24, 0x70, 0x45, 0x02, 0xea, 0x71, 0x6d, 0xba
+	};
 
-    uint8_t cipher_text[16] = {0};
-    uint8_t plain_text_decrypted[16] = {0};
+	uint8_t cipher_text[16] = {0};
+	uint8_t plain_text_decrypted[16] = {0};
 
-    mbedtls_aes_context ctx = {0};
-    mbedtls_aes_init(&ctx);
+	mbedtls_aes_context ctx = {0};
+	mbedtls_aes_init(&ctx);
 
-    // Set to use direct shadow key for encryption.
-    ret = mbedtls_aes_setkey_enc_shadow_key(&ctx, slot_id, 128);
-    if (ret != 0)
-    {
-        printk("Could not set shadow KMU ECB encrypt key.\n");
-        return -1;
-    }
-    mbedtls_aes_encrypt(&ctx, plain_text, cipher_text);
+	// Set to use direct shadow key for encryption.
+	ret = mbedtls_aes_setkey_enc_shadow_key(&ctx, slot_id, 128);
+	if (ret != 0)
+	{
+		printk("Could not set shadow KMU ECB encrypt key.\n");
+		return -1;
+	}
+	mbedtls_aes_encrypt(&ctx, plain_text, cipher_text);
 
-    if(memcmp(cipher_text, cipher_expected, 16) != 0)
-    {
-        printk("Invalid encrypted KMU ECB.\n");
-        return -1;
-    }
+	if(memcmp(cipher_text, cipher_expected, 16) != 0)
+	{
+		printk("Invalid encrypted KMU ECB.\n");
+		return -1;
+	}
 
-    // Reinitialize encryption.
-    mbedtls_aes_init(&ctx);
+	// Reinitialize encryption.
+	mbedtls_aes_init(&ctx);
 
-    // Set to use direct shadow key for decryption.
-    ret = mbedtls_aes_setkey_dec_shadow_key(&ctx, slot_id, 128);
-    if (ret != 0)
-    {
-        printk("Could not set shadow KMU ECB encrypt key.\n");
-        return -1;
-    }
+	// Set to use direct shadow key for decryption.
+	ret = mbedtls_aes_setkey_dec_shadow_key(&ctx, slot_id, 128);
+	if (ret != 0)
+	{
+		printk("Could not set shadow KMU ECB encrypt key.\n");
+		return -1;
+	}
 
-    mbedtls_aes_decrypt(&ctx, cipher_text, plain_text_decrypted);
+	mbedtls_aes_decrypt(&ctx, cipher_text, plain_text_decrypted);
 
-    if(memcmp(plain_text, plain_text_decrypted, 16) != 0)
-    {
-        printk("Invalid encrypted KMU ECB.\n");
-        return -1;
-    }
+	if(memcmp(plain_text, plain_text_decrypted, 16) != 0)
+	{
+		printk("Invalid encrypted KMU ECB.\n");
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 
@@ -115,7 +115,7 @@ int main(void)
 
 	printk("KMU sample started.\n");
 
-    if (nrf_cc3xx_platform_init() != 0)
+	if (nrf_cc3xx_platform_init() != 0)
 	{
 		printk("Failed to initialize CC3xx platform.\n");
 		return -1;
